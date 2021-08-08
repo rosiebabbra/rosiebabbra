bbe6302930d0dd267fc18070cfa2a6dbcee5fe2c
Rosie Babbra <rosie.babbra@nielsen.com>
2021-08-08 12:55:59 -0700

    Update social media information

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -138,17 +138,14 @@ export const Contact = (props) => {
               <div className='social'>
                 <ul>
                   <li>
-                    <a href={props.data ? props.data.facebook : '/'}>
+                    <a href={props.data ? props.data.instagram : '/'}
+                        target="_new">
                       <i className='fa fa-instagram'></i>
                     </a>
                   </li>
                   <li>
-                    <a href={props.data ? props.data.twitter : '/'}>
-                      <i className='fa fa-twitter'></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : '/'}>
+                    <a href={props.data ? props.data.youtube : '/'}
+                        target="_new">
                       <i className='fa fa-youtube'></i>
                     </a>
                   </li>
