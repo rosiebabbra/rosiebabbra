48e09c8beb2b1ce36828c032b905aef47a30f8bf
Rosie Babbra <rosie.babbra@nielsen.com>
2021-09-24 22:37:15 -0700

    Add code for Yelp Reviews widget - commented out for now until determine better placement

--- src/components/testimonials.jsx
+++ src/components/testimonials.jsx
@@ -1,9 +1,13 @@
+import ScriptTag from 'react-script-tag';
+
 export const Testimonials = (props) => {
   return (
     <div id='testimonials'>
       <div className='container'>
         <div className='section-title text-center'>
           <h2>What my clients say</h2>
+        </div>
+        <div>
         </div>
         <div className='row'>
           {props.data
@@ -23,6 +27,18 @@ export const Testimonials = (props) => {
               ))
             : 'loading'}
         </div>
+        {/* Yelp Review Widget */}
+        {/* <div className="row">
+          <span class="yelp-review" 
+                data-review-id="ldqVpC2ro0SmeNnUpiO3_w" 
+                data-hostname="www.yelp.com">Read 
+              <a href="https://www.yelp.com/user_details?userid=Jd9HBwlgiZPALgQ5UtALSA" rel="nofollow noopener">Atlantis B.</a>'s 
+              <a href="https://www.yelp.com/biz/rosie-babbra-santa-monica?hrid=ldqVpC2ro0SmeNnUpiO3_w" rel="nofollow noopener">review</a> of 
+              <a href="https://www.yelp.com/biz/De3QT2TtVyqN1cbmouTA8g" rel="nofollow noopener">Rosie Babbra</a> on 
+              <a href="https://www.yelp.com" rel="nofollow noopener">Yelp</a>
+              <ScriptTag src="https://www.yelp.com/embed/widgets.js" type="text/javascript" async></ScriptTag>
+              </span>
+        </div> */}
       </div>
     </div>
   )
