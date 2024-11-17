792ac604a4d14dd2401b34d2fbb26e249f9802b2
Rosie Babbra <rosiebabbra@gmail.com>
2024-11-17 13:23:55 -0600

    Updates

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,24 +1,26 @@
 export const About = (props) => {
   return (
     <div id="about">
-
-      <center>
-        <div className='section-title'>
-          <h2>About Me</h2>
-        </div></center>
       <div className="container">
-        <div className="row about-container">
-          <div className="col-xs-12 col-md-4">
-            <img src="img/about.jpg" alt="" />{" "}
+        <div className="aboutContainerShadowBox">
+
+          <div className='section-title'>
+            <center>
+              <h2>About Me</h2></center>
           </div>
-          <div className="col-xs-12 col-md-6">
-            <div className="about-text">
-              <p>{props.data ? props.data.paragraphOne : "loading..."}</p>
-              <p>{props.data ? props.data.paragraphTwo : "loading..."}</p>
-              <p>{props.data ? props.data.paragraphThree : "loading..."}</p>
-              <p>{props.data ? props.data.paragraphFour : "loading..."}</p>
-              <p>{props.data ? props.data.paragraphFive : "loading..."}</p>
-              <p>{props.data ? props.data.paragraphSix : "loading..."}  </p>
+          <div className="row about-container">
+            <div className="col-xs-12 col-md-4">
+              <img src="img/about.jpg" alt="" />{" "}
+            </div>
+            <div className="col-xs-12 col-md-6">
+              <div className="about-text">
+                <p>{props.data ? props.data.paragraphOne : "loading..."}</p>
+                <p>{props.data ? props.data.paragraphTwo : "loading..."}</p>
+                <p>{props.data ? props.data.paragraphThree : "loading..."}</p>
+                <p>{props.data ? props.data.paragraphFour : "loading..."}</p>
+                <p>{props.data ? props.data.paragraphFive : "loading..."}</p>
+                {/* <p>{props.data ? props.data.paragraphSix : "loading..."}  </p> */}
+              </div>
             </div>
           </div>
         </div>
